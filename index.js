7af26a785dc834b11a284c08c628f2bd051b8ef3
xapski2671 <xapski2671@gmail.com>
2023-11-29 05:47:08 +0100

    npm init

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const app = express()
+
+app.listen(5001, () => {
+	console.log("api running on port 5001")
+})
+
+app.get("/", (req, res) => {
+	res.json("api running on 5001⭐")
+})
