753792b3edade69f8eb6d07636f69512450f3d15
xapski2671 <xapski2671@gmail.com>
2023-11-29 08:09:54 +0100

    img update

--- controllers/getImagesController.js
+++ controllers/getImagesController.js
@@ -7,7 +7,9 @@ const imagesController = async (req, res) => {
 				status: 200,
 				success: true,
 				code: "found".toUpperCase(),
-				msg: `${req.protocol}://${req.get("host")}/awse.jpg`,
+				image:
+					"https://images.unsplash.com/photo-1700730025710-58ff304c1c8b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+				// msg: `${req.protocol}://${req.get("host")}/awse.jpg`,
 			}
 			res.send(response)
 			return
@@ -16,7 +18,8 @@ const imagesController = async (req, res) => {
 				status: 200,
 				success: true,
 				code: "found".toUpperCase(),
-				msg: `${req.protocol}://${req.get("host")}/feel_the_rhythm.jpg`,
+				image:
+					"https://images.unsplash.com/photo-1701017655822-d4f7a0569b40?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 			}
 			res.send(response)
 			return
@@ -25,7 +28,8 @@ const imagesController = async (req, res) => {
 				status: 200,
 				success: true,
 				code: "found".toUpperCase(),
-				msg: `${req.protocol}://${req.get("host")}/wqq.jpg`,
+				image:
+					"https://images.unsplash.com/photo-1518998053901-5348d3961a04?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
 			}
 			res.send(response)
 			return
